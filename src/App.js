68e57142c6e62bac03b4f2589e21532c3d7ad41a
Jonathan <jonathan.gb001@gmail.com>
2022-03-12 14:44:55 -0500

    Co-authored-by: Alfredo Dominguez <Dfredude@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">hi</div>;
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((res) => res.json())
+      .then((data) => setPokemonData(data.results));
+  }, []);
+
+  // https://pokeapi.co/api/v2/pokemon/{id}
+  // habitat https://pokeapi.co/api/v2/pokemon-habitat/{id}
+
+  return <div className="App">{pokemonData[0].name}</div>;
 }
 
 export default App;
